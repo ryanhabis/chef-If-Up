cc9b58346f328bfc5d8c9639229c3e86cb01aa4d
Ryan Habis <ryan.habiss@gmail.com>
2024-10-10 14:45:50 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace chef_If_Up.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
