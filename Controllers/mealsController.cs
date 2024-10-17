79df36a6d0f02fe76709b043afad8f80a3b1e620
Ryan Habis <ryan.habiss@gmail.com>
2024-10-17 12:46:16 +0100

    login works,

---
+++ Controllers/mealsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chef_If_Up.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mealsController : ControllerBase
+    {
+    }
+}
